9793f297d45bddca388e0ba6ab6396a20d58837f
Mreak <monyreak.kit@gmail.com>
2023-10-08 04:45:06 -0700

    Updates

---
+++ precache-manifest.99c218be16d3e952edde598394f86777.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7669a8dffd98d222a314c49168366762",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "5726e86c2d062de7dab7",
+    "url": "/home/static/css/main.491d6ec9.chunk.css"
+  },
+  {
+    "revision": "b8f78495a5d64a2da38b",
+    "url": "/home/static/js/2.639d4092.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/home/static/js/2.639d4092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5726e86c2d062de7dab7",
+    "url": "/home/static/js/main.2e5c6e3c.chunk.js"
+  },
+  {
+    "revision": "6e66bd7329b5745d98e6",
+    "url": "/home/static/js/runtime-main.87b440be.js"
+  },
+  {
+    "revision": "100665a802301b2dce725fdda472a3f2",
+    "url": "/home/static/media/engineering.100665a8.png"
+  },
+  {
+    "revision": "95b43122877503fdc7b6d01dcdece992",
+    "url": "/home/static/media/ui-design.95b43122.png"
+  }
+]);

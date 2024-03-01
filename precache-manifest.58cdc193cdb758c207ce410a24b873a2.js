def1bdd95eb85050c41e0742321208d9da19bc9e
Monyreak <mkit@ucsc.edu>
2024-03-01 06:56:44 -0800

    Updates

---
+++ precache-manifest.58cdc193cdb758c207ce410a24b873a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ba4664640795dfc12d4d892f67f72d",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "392907c749c77269517a",
+    "url": "/home/static/css/main.4f825420.chunk.css"
+  },
+  {
+    "revision": "f64cf0b260e2253d0b75",
+    "url": "/home/static/js/2.16aad331.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/home/static/js/2.16aad331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "392907c749c77269517a",
+    "url": "/home/static/js/main.167554c8.chunk.js"
+  },
+  {
+    "revision": "6e66bd7329b5745d98e6",
+    "url": "/home/static/js/runtime-main.87b440be.js"
+  },
+  {
+    "revision": "06963d0b6f4fc2fbe8038f9740070e57",
+    "url": "/home/static/media/profile.06963d0b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.2c849365547e04c255aff848c420735e.js"
+  "/home/precache-manifest.58cdc193cdb758c207ce410a24b873a2.js"
 );
 
 self.addEventListener('message', (event) => {
